dd3d1845f604d8ff1928d3c567d8bedfe7e7dbfa
alberthli <alberthli@berkeley.edu>
2024-07-17 18:12:32 -0700

    fixes

--- test_ws/src/test_pkg/src/test_node.cpp
+++ test_ws/src/test_pkg/src/test_node.cpp
@@ -1,18 +1,21 @@
 #include <rclcpp/rclcpp.hpp>
-#include "test_a_msgs/msg/test.h"
+#include "test_a_msgs/msg/test.hpp"
 
 class TestNode : public rclcpp::Node {
 public:
     TestNode() : Node("test_node") {
         pub_ = this->create_publisher<test_a_msgs::msg::Test>("test_topic", 10);
-        auto timer = this->create_wall_timer(std::chrono::seconds(1), [this]() {
-            auto msg = test_a_msgs::msg::Test();
+        timer_ = this->create_wall_timer(std::chrono::seconds(1), [this]() {
+            test_a_msgs::msg::Test msg;
             msg.header.stamp = this->now();
             pub_->publish(msg);
+            RCLCPP_INFO(this->get_logger(), "Publishing message");
         });
     }
 private:
     rclcpp::Publisher<test_a_msgs::msg::Test>::SharedPtr pub_;
+    // make a timer
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char* argv[]) {
